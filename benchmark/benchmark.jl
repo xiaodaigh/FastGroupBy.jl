38b44215e2122ac3d636582373c074e7e8845b76
susabi <zhuojia.dai@gmail.com>
2017-10-22 14:48:10 +1100

    minor reorganisation of benchmark

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -5,12 +5,14 @@ srand(1);
 const N = 10_000_000
 const K = 100
 
-@time df = DataFrame(id = rand(1:K,N), val = rand(round.(rand(K)*100,4), N))
+@time df = DataFrame(
+  id = rand(1:K,N),
+  val = rand(round.(rand(K)*100,4), N))
 # DataFrames is faster at dealing with DataArrays especially after first compilation
-@time DataFrames.aggregate(df, :id, mean)
-@time DataFrames.aggregate(df, :id, mean)
 @time meanby(df, :id, :val)
 @time meanby(df, :id, :val)
+@time DataFrames.aggregate(df, :id, mean)
+@time DataFrames.aggregate(df, :id, mean)
 
 srand(1)
 @time idt = IndexedTable(
@@ -21,14 +23,14 @@ srand(1)
   ));
 
 # meanby is faster for IndexedTables without nulls
-@time IndexedTables.aggregate_vec(mean, idt, by =(:id,), with = :val)
-@time IndexedTables.aggregate_vec(mean, idt, by =(:id,), with = :val)
 @time meanby(idt, :id, :val)
 @time meanby(idt, :id, :val)
+@time IndexedTables.aggregate_vec(mean, idt, by =(:id,), with = :val)
+@time IndexedTables.aggregate_vec(mean, idt, by =(:id,), with = :val)
 
 # meanby is also faster for DataFrame without nulls
 @time idtdf = DataFrame(idt)
-@time DataFrames.aggregate(idtdf, :id, mean)
-@time DataFrames.aggregate(idtdf, :id, mean)
 @time meanby(idtdf, :id, :val)
 @time meanby(idtdf, :id, :val)
+@time DataFrames.aggregate(idtdf, :id, mean)
+@time DataFrames.aggregate(idtdf, :id, mean)
