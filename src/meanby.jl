8307b0e35736df3a0ad0ac468f81f1325e60f685
susabi <zhuojia.dai@gmail.com>
2017-10-22 14:47:51 +1100

    using abstracttypes more

--- src/meanby.jl
+++ src/meanby.jl
@@ -1,23 +1,7 @@
 using IndexedTables
 import DataFrames.DataFrame
+import DataFrames.AbstractDataFrame
 import Base.ht_keyindex
-
-function meanby{S,T}(id4::Vector{T}, v1::Vector{S})::Dict{T,Float64}
-  res = Dict{T, Tuple{S, Int64}}()
-  szero = zero(S)
-  for (id, val) in zip(id4,v1)
-    index = ht_keyindex(res, id)
-    if index > 0
-      @inbounds vw = res.vals[index]
-      new_vw = (vw[1] + val, vw[2] + 1)
-      @inbounds res.vals[index] = new_vw
-    else
-      @inbounds res[id] = (val, 1)
-    end
-
-  end
-  return Dict(k => res[k][1]/res[k][2] for k in keys(res))
-end
 
 function meanby{S,T}(id4::AbstractArray{T,1}, v1::AbstractArray{S,1})::Dict{T,Float64}
   res = Dict{T, Tuple{S, Int64}}()
@@ -37,10 +21,10 @@ function meanby{S,T}(id4::AbstractArray{T,1}, v1::AbstractArray{S,1})::Dict{T,Fl
 end
 
 meanby(dt::IndexedTables.IndexedTable,by::Symbol, val::Symbol) = meanby(column(dt,by), column(dt,val))
-meanby(dt::DataFrame,by::Symbol, val::Symbol) = meanby(column(dt,by), column(dt,val))
+meanby(dt::AbstractDataFrame,by::Symbol, val::Symbol) = meanby(column(dt,by), column(dt,val))
 
 import IndexedTables.column
-function column(dt::DataFrame, col::Symbol)
+function column(dt::AbstractDataFrame, col::Symbol)
   i = dt.colindex.lookup[col]
   dt.columns[i]
 end
