e38ca9a9b9310ab9b312ad572ffbfa773fe7e24b
susabi <zhuojia.dai@gmail.com>
2017-10-24 22:04:33 +1100

    psumby & pmeanby

--- src/FastGroupBy.jl
+++ src/FastGroupBy.jl
@@ -1,7 +1,7 @@
 __precompile__(true)
 module FastGroupBy
 
-export meanby, column, sumby
+export meanby, column, sumby, pmeanby, psumby
 
 include("meanby.jl")
 
--- src/meanby.jl
+++ src/meanby.jl
@@ -5,6 +5,7 @@ import IndexedTables.IndexedTable
 import Base.ht_keyindex
 import IndexedTables.column
 import PooledArrays.PooledArray
+import CategoricalArrays.CategoricalArray
 # using ParallelAccelerator
 
 function meanby{S,T}(id4::AbstractArray{T,1}, v1::AbstractArray{S,1})::Dict{T,Float64}
@@ -25,15 +26,15 @@ function meanby{S,T}(id4::AbstractArray{T,1}, v1::AbstractArray{S,1})::Dict{T,Fl
 end
 
 #Optimized sumby for PooledArrays
-function meanby{S}(by::PooledArray, val::AbstractArray{S,1})
+function meanby{S}(by::Union{PooledArray, CategoricalArray}, val::AbstractArray{S,1})
   l = length(by.pool)
   res = zeros(S, l)
   wt = zeros(Int64, l)
   refs = Int64.(by.refs)
 
   for (i, v) in zip(refs, val)
-    res[i] += v
-    wt[i] += 1
+      @inbounds res[i] += v
+      @inbounds wt[i] += 1
   end
   return Dict(by.pool[i] => res[i]/wt[i] for i in 1:l)
 end
@@ -63,13 +64,13 @@ function sumby{T,S}(by::AbstractArray{T,1}, val::AbstractArray{S,1})
 end
 
 #Optimized sumby for PooledArrays
-function sumby{S}(by::PooledArray, val::AbstractArray{S,1})
+function sumby{S}(by::Union{PooledArray, CategoricalArray}, val::AbstractArray{S,1})
   l = length(by.pool)
   res = zeros(S, l)
   refs = Int64.(by.refs)
 
   for (i, v) in zip(refs, val)
-    res[i] += v
+    @inbounds res[i] += v
   end
   return Dict(by.pool[i] => res[i] for i in 1:l)
 end
@@ -86,3 +87,58 @@ end
 # end
 
 sumby(dt::Union{AbstractDataFrame, IndexedTable}, by::Symbol, val::Symbol) = sumby(column(dt,by), column(dt,val))
+
+function psumby{T,S}(by::SharedArray{T,1}, val::SharedArray{S,1})
+  np = nprocs()
+  if np == 1
+    throw(ErrorException("only one proc"))
+  end
+  l = length(by)
+  chunks = collect(Set([1:Int64(round(l/nprocs())):l...,l]))
+  ll =length(chunks)
+  res = pmap(2:ll) do i
+    j = Int64(chunks[i-1]):Int64(chunks[i])
+    sumby(by[j], val[j])
+  end
+  return res
+end
+
+function psumby{S}(by::Union{PooledArray, CategoricalArray}, val::Vector{S})
+  return sumby(by, val)
+end
+
+function psumby{T,S}(by::Vector{T}, val::Vector{S})
+  bys = SharedArray(by)
+  vals = SharedArray(val)
+  return psumby(bys, vals)
+end
+
+
+psumby(dt::Union{AbstractDataFrame, IndexedTable}, by::Symbol, val::Symbol) = psumby(column(dt,by), column(dt,val))
+
+function pmeanby{T,S}(by::SharedArray{T,1}, val::SharedArray{S,1})
+  np = nprocs()
+  if np == 1
+    throw(ErrorException("only one proc"))
+  end
+  l = length(by)
+  chunks = collect(Set([1:Int64(round(l/nprocs())):l...,l]))
+  ll =length(chunks)
+  res = pmap(2:ll) do i
+    j = Int64(chunks[i-1]):Int64(chunks[i])
+    meanby(by[j], val[j])
+  end
+  return res
+end
+
+function pmeanby{S}(by::Union{PooledArray, CategoricalArray}, val::Vector{S})
+  meanby(by, val)
+end
+
+function pmeanby{T,S}(by::Vector{T}, val::Vector{S})
+  bys = SharedArray(by)
+  vals = SharedArray(val)
+  pmeanby(bys, vals)
+end
+
+pmeanby(dt::Union{AbstractDataFrame, IndexedTable}, by::Symbol, val::Symbol) = pmeanby(column(dt,by), column(dt,val))
