34e11047fa8f13ceae1b820a4b0414dc96147443
susabi <zhuojia.dai@gmail.com>
2017-10-29 17:42:52 +1100

    copying documentation style from DataFrames.jl

--- src/FastGroupBy.jl
+++ src/FastGroupBy.jl
@@ -1,13 +1,32 @@
 __precompile__(true)
 module FastGroupBy
 
+##############################################################################
+##
+## Dependencies
+##
+##############################################################################
+
+
+##############################################################################
+##
+## Exported methods and types (in addition to everything reexported above)
+##
+##############################################################################
+
 export meanby, column, sumby, pmeanby, psumby, select, pgroupreduce
 export dict_add_reduce, dict_mean_reduce
 export sumby_htsize, sumby_sorted, sumby_dict
+
+##############################################################################
+##
+## Load files
+##
+##############################################################################
 
 include("meanby.jl")
 include("sumby.jl")
 include("util.jl")
 include("pgroupreduce.jl")
 
-end # module
+end # module FastGroupBy
--- src/sumby.jl
+++ src/sumby.jl
@@ -1,4 +1,7 @@
-# Map a bits-type to an unsigned int, maintaining sort order
+#
+#  Split - Apply - Combine - sumby operations
+#
+
 using SortingAlgorithms, Base.Order, Compat, IndexedTables
 import Base: ht_keyindex, rehash!
 import SortingAlgorithms: uint_mapping, RADIX_SIZE, RADIX_MASK
@@ -7,6 +10,52 @@ import IndexedTables: IndexedTable, column
 import PooledArrays.PooledArray
 import CategoricalArrays.CategoricalArray
 
+##############################################################################
+##
+## sumby...
+##
+##############################################################################
+
+"""
+Perform sum by group
+```julia
+sumby(df::Union{AbstractDataFrame,IndexedTable}, by::Symbol, val::Symbol)
+sumby(by::AbstractVector  val::AbstractVector)
+```
+### Arguments
+* `df` : an AbstractDataFrame/IndexedTable from which to extract the by and val columns
+* `by` : data table column to group by
+* `val`: data table column to sum
+### Returns
+* `::Dict` : A Dict that maps unqiues values of by to sum of val
+
+### Examples
+```julia
+# Generate some data
+const N = 10_000_000
+const K = 100
+srand(1)
+@time idt = IndexedTable(
+  Columns(row_id = [1:N;]),
+  Columns(
+    id = rand(1:K,N),
+    val = rand(round.(rand(K)*100,4), N)
+  ));
+
+# sumby is faster for IndexedTables without nulls
+@elapsed IndexedTables.aggregate_vec(sum, idt, by =(:id,), with = :val)
+@elapsed sumby(idt, :id, :val)
+
+# sumby is also faster for DataFrame without nulls
+@elapsed idtdf = DataFrame(idt)
+@elapsed DataFrames.aggregate(idtdf, :id, sum)
+@elapsed sumby(idtdf, :id, :val)
+
+# or you can apply directly to vectors
+@elapsed sumby(column(idt, :id), column(idt, :val))
+
+```
+"""
 function sumby{T,S}(by::AbstractVector{T},  val::AbstractVector{S})
   by_sim = similar(by)
   val1=similar(val)
@@ -99,21 +148,23 @@ function sumby_sorted{T,S}(by_sorted::AbstractVector{T},  val::AbstractVector{S}
   res
 end
 
+#sumby_sorted2 is too slow
 # function sumby_sorted2{T,S}(by_sorted::AbstractVector{T},  val::AbstractVector{S})
 #   res = Dict{T,S}()
-#   @inbounds tmp_val = val[1]
 #   @inbounds last_byi = by_sorted[1]
+#   lo = 1
+#   hi = 1
 #   @inbounds for i in 2:length(by_sorted)
 #     if val[i] == last_byi
-#       tmp_val += by_sorted[1]
+#       hi += 1
 #     else
-#       res[last_byi] = tmp_val
-#       tmp_val = val[i]
+#       res[last_byi] = sum(val[lo:hi])
 #       last_byi = by_sorted[i]
+#       lo, hi = hi + 1, hi + 1
 #     end
 #   end
-
-#   @inbounds res[last_byi] = tmp_val
+#
+#   @inbounds res[last_byi] = sum(val[lo:hi])
 #
 #   res
 # end
