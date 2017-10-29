6c4b60da54aeaf1fb9860863b5d6a7772dd60948
susabi <zhuojia.dai@gmail.com>
2017-10-29 21:11:24 +1100

    one simple sensible test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,3 +5,9 @@ using Base.Test
 # dt = createIndexedTable(1_000_000, 100);
 # @test length(sumby(dt, :id1, :v1)) == 100
 @test 1==1
+
+a = [1, 1, 2, 3, 3];
+aa = sumby(a,a);
+@test length(aa) == 3
+b = Dict(1=>2, 2=>2, 3 => 6)
+@test all([aa[k]/b[k] for k in keys(aa)] .== 1)
