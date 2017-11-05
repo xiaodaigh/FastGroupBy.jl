720f3c190c5a8566ea8c0f2bfb32ac58c90347b2
susabi <zhuojia.dai@gmail.com>
2017-11-05 13:02:25 +1100

    gitignore and better readme bench code

--- benchmark/readme_bench.jl
+++ benchmark/readme_bench.jl
@@ -1,34 +1,34 @@
-# @time using FastGroupBy
-# @time using DataFrames, IndexedTables, IterableTables
-# @time import DataFrames.DataFrame
-#
-# const N = 10_000_000
-# const K = 100
-# srand(1)
-# @time idt = IndexedTable(
-#   Columns(row_id = [1:N;]),
-#   Columns(
-#     id = rand(1:K,N),
-#     val = rand(round.(rand(K)*100,4), N)
-#   ));
-#
-# # sumby is faster for IndexedTables without nulls
-# @elapsed IndexedTables.aggregate_vec(sum, idt, by =(:id,), with = :val)
-# @elapsed IndexedTables.aggregate_vec(sum, idt, by =(:id,), with = :val)
-# @elapsed sumby(idt, :id, :val)
-# @elapsed sumby(idt, :id, :val)
-#
-# # sumby is also faster for DataFrame without nulls
-# @elapsed idtdf = DataFrame(idt)
-# @elapsed DataFrames.aggregate(idtdf, :id, sum)
-# @elapsed DataFrames.aggregate(idtdf, :id, sum)
-# @elapsed sumby(idtdf, :id, :val)
-# @elapsed sumby(idtdf, :id, :val)
-#
-# srand(1);
-# @time df = DataFrame(id = rand(1:K,N), val = rand(round.(rand(K)*100,4), N))
-# # DataFrames is faster at dealing with DataArrays especially after first compilation
-# @elapsed DataFrames.aggregate(df, :id, sum)
-# @elapsed DataFrames.aggregate(df, :id, sum)
-# @elapsed sumby(df, :id, :val)
-# @elapsed sumby(df, :id, :val)
+@time using FastGroupBy
+@time using DataFrames, IndexedTables, IterableTables
+@time import DataFrames.DataFrame
+
+const N = 10_000_000
+const K = 100
+srand(1)
+@time idt = IndexedTable(
+  Columns(row_id = [1:N;]),
+  Columns(
+    id = rand(1:K,N),
+    val = rand(round.(rand(K)*100,4), N)
+  ));
+
+# sumby is faster for IndexedTables without nulls
+@elapsed IndexedTables.aggregate_vec(sum, idt, by =(:id,), with = :val)
+@elapsed IndexedTables.aggregate_vec(sum, idt, by =(:id,), with = :val)
+@elapsed sumby(idt, :id, :val)
+@elapsed sumby(idt, :id, :val)
+
+# sumby is also faster for DataFrame without nulls
+@elapsed idtdf = DataFrame(idt)
+@elapsed DataFrames.aggregate(idtdf, :id, sum)
+@elapsed DataFrames.aggregate(idtdf, :id, sum)
+@elapsed sumby(idtdf, :id, :val)
+@elapsed sumby(idtdf, :id, :val)
+
+srand(1);
+@time df = DataFrame(id = rand(1:K,N), val = rand(round.(rand(K)*100,4), N))
+# DataFrames is faster at dealing with DataArrays especially after first compilation
+@elapsed DataFrames.aggregate(df, :id, sum)
+@elapsed DataFrames.aggregate(df, :id, sum)
+@elapsed sumby(df, :id, :val)
+@elapsed sumby(df, :id, :val)
