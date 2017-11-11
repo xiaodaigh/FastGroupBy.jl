dde300f4596e1dfad4a3b1fbf860248d296117e6
susabi <zhuojia.dai@gmail.com>
2017-11-12 01:21:28 +1100

    new king of sumby using radix-unsorted method

--- src/sumby.jl
+++ src/sumby.jl
@@ -49,14 +49,119 @@ srand(1)
 
 ```
 """
-function sumby{T, S<:Number}(by::AbstractVector{T},  val::AbstractVector{S})
+function sumby{T, S<:Number}(by::AbstractVector{T},  val::AbstractVector{S}; cutsize = 2048)
+    by_sim = similar(by)
+    val1=similar(val)
+    o = Forward
+
+    l = length(by)
+
+    if l == 0
+        return Dict{T,S}()
+    elseif l == 1
+        return Dict{T,S}(by[1] => val[1])
+    end
+
+    # Make sure we're sorting a bits type
+    if !isbits(T)
+      error("sumby_nosort on works on bits types (got $T)")
+    end
+
+    # Init
+    iters = ceil(Integer, sizeof(T)*8/RADIX_SIZE)
+
+    # distinct range to groupby over; initially the whole block is one range
+    nextranges = Tuple{Int, Int}[]
+
+    new_small_grps = similar(nextranges)
+
+    push!(nextranges, (1,l))
+
+    for j = 1:iters
+        #println(string(j,":",length(nextranges)))
+        ranges = nextranges::Array{Tuple{Int, Int}}
+        nextranges = Tuple{Int,Int}[]
+        if length(ranges) > 0
+            for (lo, hi) in ranges
+                bin = zeros(UInt32, 2^RADIX_SIZE)
+
+                # Histogram for each element, radix
+                for i = lo:hi
+                    v = uint_mapping(o, by[i])
+                    idx = @compat(Int((v >> (j-1)*RADIX_SIZE) & RADIX_MASK)) + 1
+                    @inbounds bin[idx] += 1
+                end
+
+                # Sort!
+                len = hi - lo + 1
+
+                # Unroll first data iteration, check for degenerate case
+                v = uint_mapping(o, by[hi])
+                idx = @compat(Int((v >> (j-1)*RADIX_SIZE) & RADIX_MASK)) + 1
+
+                # are all values the same at this radix?
+                if bin[idx] == len
+                    nextranges = vcat(nextranges, [(lo, hi)])::Array{Tuple{Int, Int}}
+                    by_sim[lo:hi] = by[lo:hi]
+                    val1[lo:hi] = val[lo:hi]
+                else
+                    if lo > 1;  bin[1] += UInt32(lo-1);  end
+                    cbin = cumsum(bin)
+
+                    # compute the new ranges for next round
+                    tmpr = sort(setdiff(unique(cbin), lo-1))::Vector{Int}
+                    tmpnextranges = vcat(nextranges, [(lo,Int(tmpr[1]))], [(Int(a)+1,Int(b)) for (a,b) in zip(tmpr[1:end-1],tmpr[2:end])])::Array{Tuple{Int, Int}}
+                    nextranges = filter(x -> x[2] - x[1] + 1 > cutsize, tmpnextranges)::Array{Tuple{Int, Int}}
+
+                    if length(tmpnextranges) != length(nextranges)
+                        # this is a small grp just sort it
+                        new_small_grps = filter(x -> x[2] - x[1] + 1 <= cutsize, tmpnextranges)
+                    end
+
+                    ci = cbin[idx]
+                    by_sim[ci] = by[hi]
+                    val1[ci] = val[hi]
+                    cbin[idx] -= 1
+
+                    # Finish the loop...
+                    @inbounds for i in hi-1:-1:lo
+                      v = uint_mapping(o, by[i])
+                      idx = @compat(Int((v >> (j-1)*RADIX_SIZE) & RADIX_MASK)) + 1
+                      ci = cbin[idx]
+                      by_sim[ci] = by[i]
+                      val1[ci] = val[i]
+                      cbin[idx] -= 1
+                    end
+                end
+            end
+            if length(new_small_grps) > 0
+                for (lo_nsg, hi_nsg) in new_small_grps
+                    bytmp = by_sim[lo_nsg:hi_nsg]
+                    sp = sortperm(bytmp)
+
+                    by[lo_nsg:hi_nsg] = bytmp[sp]
+                    val[lo_nsg:hi_nsg] = val1[lo_nsg:hi_nsg][sp]
+                end
+                new_small_grps = Int[]
+            end
+            by, by_sim = by_sim, by
+            val, val1 = val1, val
+        end
+    end
+
+    return sumby_contiguous(by, val)
+end
+
+
+function sumby_radix{T, S<:Number}(by::AbstractVector{T},  val::AbstractVector{S})::Dict{T,S}
   by_sim = similar(by)
   val1=similar(val)
   o = Forward
   lo = 1
   hi = length(by)
 
-  if lo >= hi;  return by;  end
+  if hi == 0;  return Dict{T,S}();
+  elseif hi == 1;  return Dict{T,S}(by[1] => val[1]);  end
 
   # Make sure we're sorting a bits type
   #TT = Base.Order.ordtype(o, by)
@@ -122,7 +227,7 @@ function sumby{T, S<:Number}(by::AbstractVector{T},  val::AbstractVector{S})
   sumby_contiguous(by, val)
 end
 
-function sumby_contiguous{T, S<:Number}(by_sorted::AbstractVector{T},  val::AbstractVector{S})
+function sumby_contiguous{T, S<:Number}(by_sorted::AbstractVector{T},  val::AbstractVector{S})::Dict{T,S}
   res = Dict{T,S}()
   @inbounds tmp_val = val[1]
   @inbounds last_byi = by_sorted[1]
@@ -163,7 +268,7 @@ end
 # end
 
 
-function sumby_dict{T,S<:Number}(by::AbstractVector{T}, val::AbstractVector{S})
+function sumby_dict{T,S<:Number}(by::AbstractVector{T}, val::AbstractVector{S})::Dict{T,S}
   res = Dict{T, S}()
   # resize the Dict to a larger size
   for (byi, vali) in zip(by, val)
