b97f0343e3527468bde21f37ad0d138c6a1031db
susabi <zhuojia.dai@gmail.com>
2017-12-28 17:51:42 +1100

    minor tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,20 @@
 using FastGroupBy
 using Base.Test
 
+tic()
 a = [1, 1, 2, 3, 3];
-aa = sumby(a,a);
-@test length(aa) == 3
-b = Dict(1=>2, 2=>2, 3 => 6)
+aa = sumby!(a,copy(a));
+aaa = fastby!(sum,a,copy(a))
+b = Dict(1 => 2, 2 => 2, 3 => 6)
 @test aa == b
+@test aaa == b
+toc()
 
+tic()
 const M=1000; const K=100
 # srand(1)
 # @time svec1 = rand(["i"*dec(k,rand(1:7)) for k in 1:M÷K], M)
 svec1 = rand([string(rand(Char.(32:126), rand(1:8))...) for k in 1:M÷K], M)
 radixsort!(svec1)
 @test issorted(svec1)
+toc()
